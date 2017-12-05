5d5e2c4c52a5a1bcc7be7508394b4576d8ec8075
anikoBarat <aniko.barat@gmail.com>
2017-12-05 16:38:33 +0100

    A basic structure for webshop processes

---
+++ src/main/java/com/codecool/shop/process/AbstractProcess.java
@@ -0,0 +1,5 @@
+package com.codecool.shop.process;
+
+public abstract class AbstractProcess {
+
+}
---
+++ src/main/java/com/codecool/shop/process/ConfirmationProcess.java
@@ -0,0 +1,4 @@
+package com.codecool.shop.process;
+
+public class ConfirmationProcess extends AbstractProcess {
+}
