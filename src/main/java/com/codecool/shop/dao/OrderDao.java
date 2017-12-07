515765b9507dbd8b1b01165c82cb7111f38f0e33
mateBedok <mate.bedok@gmail.com>
2017-12-07 22:39:41 +0100

    Minor logical errors fixed, improvements

--- src/main/java/com/codecool/shop/dao/OrderDao.java
+++ src/main/java/com/codecool/shop/dao/OrderDao.java
@@ -2,7 +2,11 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Order;
 
+import java.util.List;
+
 public interface OrderDao {
     void add(Order product);
     Order find(int id);
+
+    List<Order> getAll();
 }
