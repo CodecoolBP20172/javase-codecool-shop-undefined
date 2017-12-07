658b28e96b95ee1a86cb451da845c8faf5db7f5e
Lidia Bonifert <bonifert.lidia@gmail.com>
2017-12-07 13:16:47 +0100

    payment html looks nice

--- src/main/resources/public/JS/paymentPage.js
+++ src/main/resources/public/JS/paymentPage.js
@@ -1,38 +1,13 @@
-// var FormStuff = {
-//
-//     init: function() {
-//         this.applyConditionalRequired();
-//         this.bindUIActions();
-//     },
-//
-//     bindUIActions: function() {
-//         $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
-//     },
-//
-//     applyConditionalRequired: function() {
-//
-//         $(".require-if-active").each(function() {
-//             var el = $(this);
-//             if ($(el.data("require-pair")).is(":checked")) {
-//                 el.prop("required", true);
-//             } else {
-//                 el.prop("required", false);
-//             }
-//         });
-//
-//     }
-//
-// };
 
 function addRadioEventListeners() {
     $('#credit_card_input').hide();
     $('#paypal_input').hide();
 
-    $("input[type='radio']").on("change", function () {
-        if (this.value == 'Credit Card') {
+    $('input[type="radio"]').on('change', function () {
+        if (this.value === 'Credit Card') {
             $('#credit_card_input').show();
             $('#paypal_input').hide();
-        } else if (this.value == 'Paypal') {
+        } else if (this.value === 'Paypal') {
             $('#credit_card_input').hide();
             $('#paypal_input').show();
         }
