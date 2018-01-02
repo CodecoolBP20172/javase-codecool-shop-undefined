35ed5d2fcee149805278c961b5b2dc9ba53bc75a
anikoBarat <aniko.barat@gmail.com>
2018-01-02 17:00:51 +0100

    sql script is working, tables are created

--- src/main/resources/sql/init_db.sql
+++ src/main/resources/sql/init_db.sql
@@ -5,8 +5,8 @@ id SERIAL PRIMARY KEY NOT NULL,
 name varchar(40),
 default_price FLOAT,
 default_currency CHAR,
-supplier_id varchar(36) NOT NULL ,
-product_category_id VARCHAR(36) NOT NULL
+supplier_id INT NOT NULL ,
+product_category_id INT NOT NULL
 );
 
 DROP TABLE IF EXISTS product_category;
@@ -31,7 +31,7 @@ DROP TABLE IF EXISTS orders;
 CREATE TABLE orders
 (
 id SERIAL PRIMARY KEY NOT NULL,
-customer_id varchar(40) NOT NULL,
+customer_id INT NOT NULL,
 subtotal FLOAT
 );
 
@@ -59,24 +59,24 @@ DROP TABLE IF EXISTS line_item;
 CREATE TABLE line_item
 (
   id SERIAL PRIMARY KEY NOT NULL,
-  order_id varchar(40) NOT NULL,
-  product_id varchar(40) NOT NULL,
+  order_id INT NOT NULL,
+  product_id INT NOT NULL,
   quantity INT
 );
 
 
 
-ALTER TABLE ONLY product
+ALTER TABLE ONLY products
 ADD CONSTRAINT fk_product_category_id FOREIGN KEY (product_category_id) REFERENCES product_category(id);
 
-ALTER TABLE ONLY product
+ALTER TABLE ONLY products
 ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) REFERENCES supplier(id);
 
 ALTER TABLE ONLY orders
 ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id);
 
 ALTER TABLE ONLY line_item
-ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product(id);
+ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(id);
 
 ALTER TABLE ONLY line_item
-ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES order(id);
+ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders(id);
