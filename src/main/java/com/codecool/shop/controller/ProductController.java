9777b1c63d2a01ed0a4eb1456f09a418bf136b88
mateBedok <mate.bedok@gmail.com>
2018-01-04 15:12:49 +0100

    Minor changes in ProductCOntroller.java to support log in

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -20,6 +20,7 @@ public class ProductController {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
         Map params = new HashMap<>();
+        params.put("authentication", false);
         params.put("category", productCategoryDataStore.find(1));
         params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         return new ModelAndView(params, "product/index");
