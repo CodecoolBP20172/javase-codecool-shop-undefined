c3a8801ba8a714aff965c0e73a2a4c014a7a2a1c
mateBedok <mate.bedok@gmail.com>
2018-01-17 13:20:29 +0100

    Request handling methods added

---
+++ src/main/java/com/codecool/shop/utils/RequestUtil.java
@@ -0,0 +1,22 @@
+package com.codecool.shop.utils;
+
+import com.codecool.shop.model.Customer;
+import spark.Request;
+
+public class RequestUtil {
+
+    public static Integer getCustomerIdFromSession(Request req) {
+        if (req.session().attribute("currentUser") != null) {
+            return req.session().attribute("currentUser");
+        }
+        return null;
+    }
+
+    public static void setCustomerIdToSession(Request req, Customer customer) {
+        req.session().attribute("currentUser", customer.getId());
+    }
+
+    public static void removeSessionAttribute(Request req) {
+        req.session().removeAttribute("currentUser");
+    }
+}
