44d77f1121a61dff50f3bd66dcec5a68c8c57d2b
Lidia Bonifert <bonifert.lidia@gmail.com>
2018-01-18 16:17:13 +0100

    Controller methods implemented

--- src/main/java/com/codecool/shop/controller/SortingController.java
+++ src/main/java/com/codecool/shop/controller/SortingController.java
@@ -11,46 +11,22 @@ import spark.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.codecool.shop.utils.RequestUtil.getCustomerIdFromSession;
-import static com.codecool.shop.utils.RequestUtil.getProductCategoryIdFromSession;
-import static com.codecool.shop.utils.RequestUtil.setCustomerIdToSession;
+import static com.codecool.shop.utils.RequestUtil.*;
 
 public class SortingController {
 
 
-    private static Boolean isSortingValid(Request req) {
-        /*ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
-
-        if (productCategoryDataStore.getAllNames()) {
-            return false;
-        }
-        CustomerDao customerDataStore = CustomerDaoJdbc.getInstance();
-        Customer customer = customerDataStore.getCustomerByEmail(email);
-        if (customer == null) {
-            return false;
-        }
-        String hashedPassword = BCrypt.hashpw(password, customer.getSalt());
-        if (hashedPassword.equals(customer.getHashedPassword())) {
-            setCustomerIdToSession(req, customer);
-            return true;
-        }
-        throw new IllegalArgumentException("Illegal argument!");
-    }
-*/
-        return null;
-    }
-
-
     public static ModelAndView renderProductCategory(Request req, Response res) {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
-        req.session().attribute("productCategoryId", productCategoryDataStore.getIdByName(req.params(":name")));
+        setProductCategoryIdToSession(req, productCategoryDataStore.getIdByName(req.params(":name")));
         res.redirect("/");
         return null;
+    }
 
-
-
-
-
+    public static ModelAndView renderAllProductCategory(Request req, Response res) {
+        removeProductCategoryIdAttribute(req);
+        res.redirect("/");
+        return null;
     }
 
 
